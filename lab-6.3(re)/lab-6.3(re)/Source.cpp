4623fa681163ce5ac9c1399dfc563a6e50c445bd
Yuliia <ylua.vasylyk.04@gmail.com>
2021-11-08 12:10:23 +0200

    hello

--- lab-6.3(re)/lab-6.3(re)/Source.cpp
+++ lab-6.3(re)/lab-6.3(re)/Source.cpp
@@ -44,6 +44,38 @@ void Print(T* a, const int size, int i)
 		cout << endl;
 }
 
+int Index(int* a, const int size, int i)
+{
+	if (i < size)
+	{
+		if (a[i] % 2 == 0)
+			return i;
+		Index(a, size, i + 1);
+	}
+	else
+		if (a[i] % 2 == 0)
+			return 0;
+		else
+			return -1;
+}
+
+template<typename T>
+T Index(T* a, const int size, int i)
+{
+	if (i < size)
+	{
+		if (a[i] % 2 == 0)
+			return i;
+		Index(a, size, i + 1);
+	}
+	else
+		if (a[i] % 2 == 0)
+			return 0;
+		else
+			return -1;
+}
+
+
 int Max(int* a, const int size, int max, int i)
 {
 	if (a[i] > max && a[i] % 2 == 0)
@@ -78,7 +110,8 @@ int main()
 	cout << "High = "; cin >> High;
 	Create<int>(a, n, Low, High, 0);
 	Print<int>(a, n, 0);
-	int max = Low + 1;
+	int max = Index(a, n, 0);
+	if (max!= -1)
 	cout << "max = " << Max<int>(a, n, max, 0) << endl;
 	delete[] a;
 	a = nullptr;
